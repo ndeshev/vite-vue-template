f6c632c48f6e430790602a62ca176f5065e9da3f
Deshev <nikolay.deshev@sap.com>
2023-11-07 15:56:45 +0200

    Add ui5-webcomponents examples to the welcome screen

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { ref } from 'vue'
+import '@ui5/webcomponents/dist/Title'
+import '@ui5/webcomponents/dist/Label'
 
 defineProps({
   msg: String,
@@ -9,11 +11,10 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <ui5-title>{{ msg }}</ui5-title>
+  <ui5-label> Generate your Vue application with UI5 Web Components</ui5-label>
 
-  <p> Generate your Vue application with UI5 Web Components</p>
-
-  <p class="read-the-docs">Click on the Vite, Vue and UI5 WebComponents logogs to learn more</p>
+  <p class="read-the-docs">Click on the Vite, Vue and UI5 WebComponents logos to learn more</p>
 </template>
 
 <style scoped>
